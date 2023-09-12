15bce5c6946c8ffd7ce5722375c2a82e779b9a88
ileneramirez <141769717+ileneramirez@users.noreply.github.com>
2023-09-12 11:57:18 -0700

    Add files via upload

---
+++ Sample Zoo.java
@@ -0,0 +1,21 @@
+// Ilene Ramirez
+// 9/12/23
+
+// Midterm
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome t");
+
+        ArrayList<String> Animals = new ArrayList<>();
+        //Add Animals
+        Animals.add("")
+
+
+
+
+        }
+    }
