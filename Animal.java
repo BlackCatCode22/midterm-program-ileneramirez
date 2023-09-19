28ab1c12cc345720689793a36f391db7da2a7e04
ileneramirez <141769717+ileneramirez@users.noreply.github.com>
2023-09-19 12:20:00 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,2 +1,11 @@
 public class Animal {
+    private static final int numOfAnimals = 0;
+
+    String desc;
+    String birthSeason;
+    String color;
+    String weight;
+    String origin01;
+    String origin02;
+
 }
